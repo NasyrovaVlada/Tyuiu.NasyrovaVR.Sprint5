9ef92a72786d39950b1b350879c515a21a6d8d00
NasyrovaVlada <Tyuiu.NasyrovaVR@outlook.com>
2023-11-16 19:17:56 +0500

    Task2

---
+++ Tyuiu.NasyrovaVR.Sprint5.Task2.V6.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.NasyrovaVR.Sprint5.Task2.V6.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.NasyrovaVR.Sprint5.Task2.V6.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("2b131bae-f6fd-4c98-ae49-9315fbe2ccdb")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Tyuiu.NasyrovaVR.Sprint5.Task2.V6/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Tyuiu.NasyrovaVR.Sprint5.Task2.V6.Lib;
+
+namespace Tyuiu.NasyrovaVR.Sprint5.Task2.V6
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] mtrx = new int[3, 3]{ { -2, 1, 8 },
+                                         { -4, -7, 8 },
+                                         { 6, 5, 5 } };
+            int rows = mtrx.GetUpperBound(0) + 1;
+            int colums = mtrx.Length / rows;
+
+            DataService ds = new DataService();
+
+
+            Console.Title = "Спринт #5 | Выполнила: Насырова В. Р. | ИИПб-23-3";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #5                                                               *");
+            Console.WriteLine("* Тема: Класс File. Запись структурированных данных в текстовый файл      *");
+            Console.WriteLine("* Задание #2                                                              *");
+            Console.WriteLine("* Вариант #6                                                              *");
+            Console.WriteLine("* Выполнила: Насырова Влада Ренатовна | ИИПб-23-3                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Дан двумерный целочисленный массив 3 на 3 элементов, заполненный        *");
+            Console.WriteLine("* значениями с клавиатуры. Заменить положительные элементы массива на 1,  *");
+            Console.WriteLine("* отрицательные на 0. Результат сохранить в файл OutPutFileTask2.csv и    *");
+            Console.WriteLine("* вывести на консоль.                                                     *");
+            Console.WriteLine("* -2; 1; 8                                                                *");
+            Console.WriteLine("* -4; -7; 8                                                               *");
+            Console.WriteLine("* 6; 5; 5                                                                 *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("Массив: ");
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j<colums; j++)
+                {
+                    Console.Write($"{mtrx[i, j]} \t");
+                }
+                Console.WriteLine();
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            string res = ds.SaveToFileTextData(mtrx);
+
+            Console.WriteLine("Файл " + res);
+            Console.WriteLine("Создан!");
+            Console.ReadKey();
+        }
+    }
+}
